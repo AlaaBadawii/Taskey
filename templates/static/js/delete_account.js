4b5fb4e82a7b62aa70dd92bd4cda4c42cc0440f6
AlaaBadawii <alaaahmed16112000@gmail.com>
2024-05-22 13:37:11 +0300

    check

--- templates/static/js/delete_account.js
+++
@@ -1,7 +0,0 @@
-function confirmDelete(event) {
-    event.preventDefault();
-    console.log("confirmDelete() function is called.");
-    if (confirm("Are you sure you want to delete your account?")) {
-        document.getElementById('delete-user-form').submit();
-    }
-}
